f04f5c8fc788e3404b41a9bfa46e5875d6750bc8
jekkiboi <hollymayer4@gmail.com>
2022-09-18 18:27:57 -0700

    my first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
+import { Link } from 'react-router-dom'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <h1 className='title'>With Love, From Jack</h1>
         </p>
+        <img className='bgme'src={`${process.env.PUBLIC_URL}/photos/videogame.jpg`} alt="vg"/>
+        <br />
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.tiktok.com/@withlovefromjack?_t=8Vm5OnSsoGI&_r=1"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          TikTok 
         </a>
-      </header>
+        <br />
+        <a
+          className="App-link"
+          href="https://www.instagram.com/withlovefromjack/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Instagram 
+        </a>
+        <br />
+        <a
+          className="App-link"
+          href="https://soundcloud.com/jekkiboi"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Soundcloud 
+        </a>
+        <br/>
+        <a
+          className="App-link"
+          href="https://open.spotify.com/track/4sVUx7LZGNGAUIH99y0CrI?si=8UD7_DHkTYK5IXfctdkjoA"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Spotify 
+        </a>
+
     </div>
   );
 }
